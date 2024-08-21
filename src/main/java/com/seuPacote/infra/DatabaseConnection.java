723c5a487cc20e9ee73387d7f7a581268c1ccdb9
João Vitor Souza <161721838+srjoaovitorsouzas@users.noreply.github.com>
2024-08-21 16:27:04 -0300

    Adicionadas as configurações do banco de dados e corrigidos problemas

--- src/main/java/com/seuPacote/infra/DatabaseConnection.java
+++ src/main/java/com/seuPacote/infra/DatabaseConnection.java
@@ -1,13 +1,13 @@
-package com.seuPacote.infra;
+package infra;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/pizzaria";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+public class DataBaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/pizzaria"; // Nome do banco de dados
+    private static final String USER = "root"; // Usuário padrão do MySQL
+    private static final String PASSWORD = ""; // Senha padrão do MySQL, geralmente vazia
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
