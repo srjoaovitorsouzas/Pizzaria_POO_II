778b085cc07958fdc528ca4f565e3ade50998dc5
João Vitor Souza <161721838+srjoaovitorsouzas@users.noreply.github.com>
2024-11-09 13:57:13 -0300

    Initial commit

--- src/main/java/com/seuPacote/infra/DatabaseConnection.java
+++ src/main/java/com/seuPacote/infra/DatabaseConnection.java
@@ -1,13 +1,13 @@
-package infra;
+package com.seuPacote.infra;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/pizzaria"; // Nome do banco de dados
-    private static final String USER = "root"; // Usuário padrão do MySQL
-    private static final String PASSWORD = ""; // Senha padrão do MySQL, geralmente vazia
+    private static final String URL = "jdbc:mysql://192.168.1.246:3306/pizzaria";
+    private static final String USER = "userpizzaria";
+    private static final String PASSWORD = "#USERpizza06";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
